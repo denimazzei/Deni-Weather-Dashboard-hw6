6d15d2dd9b572f4390451ee53929df386aa420e5
Deni Mazzei <dconkell@gmail.com>
2021-09-03 12:13:34 -0500

    fix to 5day forecast

--- Assets/JavaScript/javascript.js
+++ Assets/JavaScript/javascript.js
@@ -22,7 +22,7 @@ function find(c) {
 
 //API Key//
 
-var APIKey = "a0aca8a89948154a4182dcecc780b513"
+var APIKey = "a0aca8a89948154a4182dcecc780b513";
 
 //Display current and future weather based on city input//
 
@@ -40,12 +40,12 @@ function currentWeather(city) {
     var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
     $.ajax({
         url:queryURL,
-        method:"GET"
+        method:"GET",
     }).then(function(response){
         console.log(response);
 
         var weatherIcon = response.weather[0].icon;
-        var iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon +"@2x.png";
+        var iconUrl = "https://openweathermap.org/img/wn/" +weatherIcon+"@2x.png";
         var date = new Date(response.dt*1000).toLocaleDateString();
         $(currentCity).html(response.name + "("+date+")" + "<img src="+iconUrl+">");
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
@@ -54,7 +54,7 @@ function currentWeather(city) {
         var ws = response.wind.speed;
         var windsmph = (ws*2.237).toFixed(1);
         $(currentWindSpeed).html(windsmph+"MPH");
-        currentUvIndex(response.coord.lon,response.coord.lat);
+        UVIndex(response.coord.lon,response.coord.lat);
         forecast(response.id);
         if(response.cod==200){
             sCity = JSON.parse(localStorage.getItem("cityname"));
@@ -80,7 +80,7 @@ function UVIndex(ln,lt) {
     var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat="+lt+"&lon="+ln;
     $.ajax({
         url:uvqURL,
-        method:"GET"
+        method:"GET",
     }).then(function(response){
         $(currentUvIndex).html(response.value);
     });   
@@ -92,7 +92,7 @@ function forecast(cityid) {
     var queryforecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" +cityid+"&appid="+APIKey;
     $.ajax({
         url: queryforecastURL,
-        method:"GET"
+        method:"GET",
     }).then(function(response){
         for (i=0;i<5;i++){
             var date = new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
